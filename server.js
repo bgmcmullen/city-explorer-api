a41b9082858ceb60fc33a4ab4af196a7c60c7a3e
Brendan <bgmcmullen@comcast.net>
2024-03-26 15:03:35 -0400

    movies live

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ const weather = require('./data/weather.json');
 dotenv.config();
 const PORT = process.env.PORT;
 const WEATHER_API_KEY=process.env.WEATHER_API_KEY;
+const MOVIE_API_KEY=process.env.MOVIE_API_KEY;
 
 const app = express(); // create our express app, now we are ready to define some functionality.
 app.use(cors()); // activates cross-origin-resource-sharing. allow other origins (besides localhost to make request to this code).
@@ -28,28 +29,48 @@ class Forecast {
   }
 }
 
+class Movie {
+    constructor(title, overview, vote_average, vote_count, popularity, release_date){
+    this.title = title;
+    this.overview = overview;
+    this.vote_average = vote_average;
+    this.vote_count = vote_count;
+    this.popularity = popularity;
+    this.release_date = release_date;
+  }
+}
+
 app.get('/weather', async (request, response) => {
       // Extract query parameters: lat, lon, and searchQuery
-  let city;
-  let forecastArray = [];
       const {lat, lon, searchQuery} = request.query;
-  // if(lat)
-  //   city = weather.find((element) => element.lat === lat);
-  // if(lon)
-  //   city = weather.find((element) => element.lon=== lon);
-  // if(searchQuery)
-  //   city = weather.find((element) => element.city_name === searchQuery);
+
     const weatherResponse = await axios.get(`https://api.weatherbit.io/v2.0/current?lat=${lat}&lon=${lon}&key=${WEATHER_API_KEY}`)
 
-    console.log(weatherResponse.data.data[0].datetime, weatherResponse.data.data[0].weather, weatherResponse.data.data[0].app_temp);
 
 
-  // if(city === undefined){
-  //   throw new Error('Invalid City');
-  // }
 
-    response.send(new Forecast(weatherResponse.data.data[0].datetime, weatherResponse.data.data[0].app_temp, weatherResponse.data.data[0].weather.description));
+    response.send({weather: new Forecast(weatherResponse.data.data[0].datetime, weatherResponse.data.data[0].app_temp, weatherResponse.data.data[0].weather.description)});
   })
+
+  app.get('/movies', async (request, response) => {
+    // Extract query parameters: lat, lon, and searchQuery
+let city;
+let forecastArray = [];
+    const {lat, lon, searchQuery} = request.query;
+
+
+
+  const movieReponse = await axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${MOVIE_API_KEY}&query=${searchQuery}`);
+
+  console.log(movieReponse.data.results);
+
+  let moviesArray = movieReponse.data.results.map(element => new Movie(element.title, element.overview, element.vote_average , element.vote_count, element.popularity, element.release_date));
+
+ 
+
+  response.send({
+    movies: moviesArray});
+})
 
 app.listen(PORT, () => {
   console.log('App is listening!!');
