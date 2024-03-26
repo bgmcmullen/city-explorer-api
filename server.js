9e30188c433678112b63aa04b09411fe995f735a
Brendan <bgmcmullen@comcast.net>
2024-03-26 10:50:54 -0400

    connected to weather API

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 'use strict';
+const axios = require('axios');
 
 const dotenv = require('dotenv');
 // import express from 'express';
@@ -10,6 +11,7 @@ const weather = require('./data/weather.json');
 
 dotenv.config();
 const PORT = process.env.PORT;
+const WEATHER_API_KEY=process.env.WEATHER_API_KEY;
 
 const app = express(); // create our express app, now we are ready to define some functionality.
 app.use(cors()); // activates cross-origin-resource-sharing. allow other origins (besides localhost to make request to this code).
@@ -19,33 +21,34 @@ response.send("Hello world!")
 })
 
 class Forecast {
-  constructor(date, description){
+  constructor(date, temperature, description){
     this.date = date;
+    this.temperature = temperature;
     this.description = description;
   }
 }
 
-app.get('/weather', (request, response) => {
+app.get('/weather', async (request, response) => {
       // Extract query parameters: lat, lon, and searchQuery
   let city;
   let forecastArray = [];
       const {lat, lon, searchQuery} = request.query;
-  if(lat)
-    city = weather.find((element) => element.lat === lat);
-  if(lon)
-    city = weather.find((element) => element.lon=== lon);
-  if(searchQuery)
-    city = weather.find((element) => element.city_name === searchQuery);
+  // if(lat)
+  //   city = weather.find((element) => element.lat === lat);
+  // if(lon)
+  //   city = weather.find((element) => element.lon=== lon);
+  // if(searchQuery)
+  //   city = weather.find((element) => element.city_name === searchQuery);
+    const weatherResponse = await axios.get(`https://api.weatherbit.io/v2.0/current?lat=${lat}&lon=${lon}&key=${WEATHER_API_KEY}`)
 
-    forecastArray= city.data.map((element) => {
-      return new Forecast(element.datetime, element.weather.description);
-    }) 
+    console.log(weatherResponse.data.data[0].datetime, weatherResponse.data.data[0].weather, weatherResponse.data.data[0].app_temp);
 
-  if(city === undefined){
-    throw new Error('Invalid City');
-  }
 
-    response.send(forecastArray);
+  // if(city === undefined){
+  //   throw new Error('Invalid City');
+  // }
+
+    response.send(new Forecast(weatherResponse.data.data[0].datetime, weatherResponse.data.data[0].app_temp, weatherResponse.data.data[0].weather.description));
   })
 
 app.listen(PORT, () => {
